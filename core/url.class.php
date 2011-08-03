65f84e67d758d41f083e975cd43b6f2378c248a8
Ralph Cowling <acastofthousands@gmail.com>
2011-08-03 23:24:49 +0100

    er having a bash at adding some stuff into the url.class in the core. I doubt this is the right place for this...

--- core/url.class.php
+++ core/url.class.php
@@ -62,4 +62,23 @@ class Url {
         // Add the host
         return "http://" . $host . $url;
     }
+    
+    /**
+     * Basic URL validation checker for use with e.g. a basic shortening service
+     * A-la http://mathiasbynens.be/demo/url-regex
+     * //WHEARE DO HELPERS GO?
+     * e.g. http:// shouldfail.com returns false, http://shouldfail.com returns true. 
+     * No curl look-up gubbins.
+    **/
+    
+    static function checkUrl($url)
+    {
+    	$check = preg_match('_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS', trim($url));
+    	
+    	if($check){
+    		return $url;
+    	} else {
+    		return false; //is there some kind of exception to throw? harumph.
+    	}
+    }
 }
